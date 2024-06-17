3492aed50cbbe399853acaa7ba4bc757a349981c
IOANNIS XIRADAKIS <csd4969@csd.uoc.gr>
2024-06-17 17:13:59 +0300

    Check if I have already seen stores for this query to save requests

--- react_challenge/src/SearchBar.js
+++ react_challenge/src/SearchBar.js
@@ -1,9 +1,32 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { setElements } from './globals';
+import { setElements, elementsHasKey } from './globals';
 import { useSearch } from './SearchContext';
 import './SearchBar.css';
 
+//function getTestData() {
+//  return [
+//    {
+//      name: "Silfio",
+//      address: "Leof. Dimokratias 73, Iraklio 713 06, Greece",
+//      city: "Heraklion",
+//      rating: 4.6
+//    },
+//    {
+//      name: "Red Corner",
+//      address: "Leof. Knosou, Knosos 714 09, Greece",
+//      city: "Knossos",
+//      rating: 4.2
+//    },
+//    {
+//      name: "Pizza Fan Ηράκλειο Κρήτης 2",
+//      address: "Leof. Papanastasiou 89, Iraklio 714 09, Greece",
+//      city: "Heraklion",
+//      rating: 3.5
+//    },
+//    // Add more entries as needed
+//  ];
+//}
 
 function SearchBar() {
   const [query, setQuery] = useState('');
@@ -11,39 +34,47 @@ function SearchBar() {
 
   const handleSearch = () => {
     if (query.trim() !== '') {
-      setSearchKey(query);
-      fetchDataAndCreateCards(query);
+      setSearchKey(query.trim());
+      fetchDataAndCreateCards(query.trim());
       setQuery('');
     }
   };
 
   const fetchDataAndCreateCards = async (searchQuery) => {
     try {
-      const response = await axios.get('https://local-business-data.p.rapidapi.com/search-in-area', {
-        headers: {
-          'X-Rapidapi-Key': '2925e31e47mshb642e62939bdb2bp13809ajsn0fc8881a286b',
-          'X-Rapidapi-Host': 'local-business-data.p.rapidapi.com'
-        },
-        params: {
-          query: searchQuery,
-          lat: '35.3387', // Latitude for Heraklion, Crete
-          lng: '25.1442', // Longitude for Heraklion, Crete
-          zoom: '13' // Zoom level
-        }
-      });
+      console.log(elementsHasKey(searchQuery.trim()));
+      if(!elementsHasKey(searchQuery.trim())) {
+       const response = await axios.get('https://local-business-data.p.rapidapi.com/search-in-area', {
+         headers: {
+           'X-Rapidapi-Key': '2925e31e47mshb642e62939bdb2bp13809ajsn0fc8881a286b',
+           'X-Rapidapi-Host': 'local-business-data.p.rapidapi.com'
+         },
+         params: {
+           query: searchQuery,
+           lat: '35.3387', // Latitude for Heraklion, Crete
+           lng: '25.1442', // Longitude for Heraklion, Crete
+           zoom: '13' // Zoom level
+         }
+       });
 
-      // Extract the required data from the API response
-      const extractedData = response.data.data.map(store => ({
-        name: store.name,
-        address: store.address,
-        city: store.city,
-        state: store.state,
-        zip_code: store.zip_code,
-        rating: store.rating
-      }));
+       // Extract the required data from the API response
+       const extractedData = response.data.data.map(store => ({
+         name: store.name,
+         address: store.address,
+         city: store.city,
+         state: store.state,
+         zip_code: store.zip_code,
+         rating: store.rating
+       }));
+//        const extractedData = getTestData();
 
-      setElements(extractedData, searchQuery); // Store the extracted data in a global variable
-      document.dispatchEvent(new CustomEvent('dataFetched')); // Trigger an event to notify data is fetched
+        setElements(extractedData, searchQuery.trim()); // Store the extracted data in a global variable
+        document.dispatchEvent(new CustomEvent('dataFetched')); // Trigger an event to notify data is fetched
+      }
+      else {
+        console.log("exists");
+      }
+
     } catch (error) {
       console.error('Error fetching data:', error);
     }
